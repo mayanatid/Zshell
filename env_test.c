88fb399ffc1a1e67aa7524fe91793f4464265ab4
mayan atid <atid_m@qwasar.io>
2023-02-15 01:02:57 +0000

    working on fork

--- env_test.c
+++ env_test.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <dirent.h>
 #include <sys/types.h>
+
+#define MAX_BUFFER 1024
 
 bool cmnd_in_dir(char* dir, char* cmnd)
 {
@@ -57,26 +60,143 @@ char* read_path(char* path, char* cmnd)
     return NULL;
 }
 
-int main(int ac, char* av[], char* env[])
+char** construct_arg_list(int ac, char* av[])
 {
-
-
-    char* path = read_path(getenv("PATH"), av[1]);
-    if(!path)
+    char** argList = (char**)malloc(sizeof(char*) * (ac));
+    int i =1;
+    while(i < ac)
     {
-        printf("Couldn't find command!\n");
+        argList[i-1] = (char*)malloc(strlen(av[i]) + 1);
+        memset(argList[i-1], 0, strlen(av[i]) + 1);
+        strcpy(argList[i-1], av[i]);
+        i++;
+    }
+    argList[i - 1] = NULL;
+    return argList;
+}
+
+int get_number_arguments(char* input)
+{
+    int arg_count = 0;
+    int i = 0;
+    if(input[0] == '\0')
+    {
         return 0;
     }
-    // printf("Found ls in %s\n", path);
-    strcat(path, "/");
-    strcat(path, av[1]);
-    char* arglist[] = {path, "ls", NULL};
-    char* envList[] = {"HOME=/root", getenv("PATH"), NULL};
-    execve(arglist[0], arglist, envList);
+    arg_count++;
+    while (input[i] != '\0')
+    {
+        if(input[i] == ' ')
+        {
+            arg_count++;
+        }
+        i++;
+    }
+    return arg_count;
+}
+
+char** construct_arg_list_from_input(char* input)
+{
+    int arg_count = get_number_arguments(input);
+    char** argList = (char**)malloc(sizeof(char*) * (arg_count + 1));
+    int i_start= 0;
+    int i_end = 0;
+    int j = 0;
+
+    while(true)
+    {
+        if(input[i_end] == ' ' || input[i_end] == '\0')
+        {
+            argList[j] = (char*)malloc(i_end - i_start + 1);
+            memset(argList[j], 0, i_end - i_start + 1);
+            strncpy(argList[j], &input[i_start], i_end);
+            i_start = i_end + 1;
+            j++;
+        }
+        if(input[i_end] == '\0')
+        {
+            return argList;
+        }
+        i_end++;
+    }
+
+    return NULL;
+
+}
+
+void free_arg_list(int ac, char** argList)
+{
+    int i =0;
+    while(i < ac)
+    {
+        free(argList[i]);
+    }
+    free(argList);
+}
+
+int main(int ac, char* av[], char* env[])
+{
+    char buffer[MAX_BUFFER];
+    // char** argList_av = construct_arg_list(ac, av);
+    // char* path = read_path(getenv("PATH"), argList_av[0]);
+    // if(!path)
+    // {
+    //     printf("Couldn't find command!\n");
+    //     return 0;
+    // }
+
+    while(true)
+    {   
+        pid_t pid, wpid;
+        int status;
+
+        pid = fork();
+        if(pid == 0)
+        {
+            memset(buffer, 0, MAX_BUFFER);
+            printf("MA - my_zsh>");
+            scanf("%s", buffer);
+            char** argList = construct_arg_list_from_input(buffer);
+            char* path = read_path(getenv("PATH"), argList[0]);
+
+            if(!path)
+            {
+                printf("Couldn't find command!\n");
+                return 0;
+            }
+
+            strcat(path, "/");
+            strcat(path, argList[0]);
+            argList[0] = (char*)realloc(argList[0], strlen(path) + 1);
+            memset(argList[0], 0,  strlen(path) + 1);
+            strcpy(argList[0], path);
+            char* envList[] = {"HOME=/root", getenv("PATH"), NULL};
+            if(execve(argList[0], argList, envList) == -1)
+            {
+                perror("lsh");
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                do {
+                    wpid = waitpid(pid, &status, WUNTRACED);
+                } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+            }
+        }        
+        return 1;
+    }
+
+    // strcat(path, "/");
+    // strcat(path, argList[0]);
+    // argList[0] = (char*)realloc(argList[0], strlen(path) + 1);
+    // memset(argList[0], 0,  strlen(path) + 1);
+    // strcpy(argList[0], path);
+    // char* envList[] = {"HOME=/root", getenv("PATH"), NULL};
+    // execve(argList[0], argList, envList);
     
     
-    
-    free(path);
+    // free_arg_list(ac, argList);
+    // free(path);
     // cmnd_in_dir("/home/docode/.goenv/versions/1.18.7/bin", "ls");
 
     // printf("env[0] = %s\n", env[0]);
